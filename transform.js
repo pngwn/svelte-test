3ab17371586d5d4b9fa633c9b1f8e21d52e9af7f
pngwn <hello@pngwn.io>
2019-05-20 00:15:10 +0100

    I forgot to actually fix the broken stuff.

--- transform.js
+++ transform.js
@@ -1,13 +1,22 @@
 const svelte = require('svelte/compiler');
 const { execSync } = require('child_process');
+const { resolve } = require('path');
 
 module.exports = {
   process: (source, filePath, config, s) => {
+
+    const preprocess = resolve(__dirname, './preprocess.js');
     
-    process.env.sveltest = source;
-    const processed = execSync(`node ./preprocess.js`).toString();
-    const compiled = svelte.compile(processed, {format: 'cjs'});
+    let processed = source;
+    if (config.globals && config.globals.svelte && config.globals.svelte.preprocess) {
+      process.env.sveltest = source;
+      processed = execSync(`node ${preprocess}`).toString();
+    }
   
-    return compiled.js.code;
+    const compiled = svelte.compile(processed, {format: 'cjs'});
+
+    return `${compiled.js.code} 
+    module.exports = exports.default;
+    `;
   }
 };
